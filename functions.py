Correct errors in coords calculation

functions.py
@@ -40,12 +40,12 @@ def make_wind_farm_coords(S_x, S_y, S_off, theta):
 
     Returns
     -------
-    farm_coords: ndarray of shape (7, 7)
+    farm_coords: ndarray of shape (7, 7, 2)
         The x, y coordinates of the closest 49 wind turbines
     """
-    farm_coords = np.zeros(7, 7)
+    farm_coords = np.zeros((7, 7, 2))
     for n_y in np.arange(-3, 4):
-        farm_coords[n_y, :] = [calculate_turbine_coords(S_x, S_y,
+        farm_coords[n_y, :, :] = [calculate_turbine_coords(S_x, S_y,
             S_off, theta, n_x, n_y) for n_x in np.arange(-3, 4)]
     return farm_coords
 
@@ -71,7 +71,7 @@ def calculate_turbine_coords(S_x, S_y, S_off, theta, n_x, n_y):
 
     Returns
     -------
-    turbine coords: ndarray of shape (2,)
+    turbine coords: tuple of shape (2,)
         The x, y coordinates of the wind turbine specified by
         the turbine numbers n_x, n_y
     """
@@ -79,4 +79,4 @@ def calculate_turbine_coords(S_x, S_y, S_off, theta, n_x, n_y):
         - np.sin(theta)*S_y*n_y
     y = np.sin(theta)*S_x*n_x + np.sin(theta)*S_off*n_y \
         + np.cos(theta)*S_y*n_y
-    return np.array([[x, y]])
+    return (x, y)